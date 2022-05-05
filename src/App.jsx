abd143ce3cc5cc6a7a590c5865e0d0007f101839
TrevorRezac <trev.rezac@gmail.com>
2022-05-05 10:55:36 -0700

    set up basic Auth & Home views, created Routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
+import { Route, Switch } from 'react-router-dom';
+import Home from './views/Home/Home';
+import Auth from './views/Auth/Auth';
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Switch>
+      <Route exact path='/auth'>
+        <Auth />
+      </Route>
+      <Route exact path='/'>
+        <Home />
+      </Route>
+    </Switch>
+  );
 }
